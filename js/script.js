f804dbf0964273dea4c5f17cbd366a0b5e6a7500
devcamila <camilacerda3@gmail.com>
2021-06-02 16:14:09 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
-    alert("Em breve, te enviaremos novidades!")
-} else {
-    alert("Por favor, preencha os campos: nome e email!")
+var bemVindo = prompt("Bem vinde, qual seu nome?")
+alert(bemVindo+ ", tenha uma ótima leitura")
+
+
+document.getElementById("enviar").addEventListener("click", respostaForm)
+
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var celular = document.getElementById("cel");
+
+function respostaForm() {
+    if (nome.value != "" && email.value != "" &&  celular.value != "") {
+        alert("Em breve, te enviaremos novidades!")
+    } else {
+        alert("Por favor, preencha os campos: nome, email e celular!")
+    }
 }
 
