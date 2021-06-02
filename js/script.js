731e5a781e37b7f139343a09ed18e3e46142a022
devcamila <camilacerda3@gmail.com>
2021-06-01 21:42:31 -0300

    add js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
+    alert("Em breve, te enviaremos novidades!")
+} else {
+    alert("Por favor, preencha os campos: nome e email!")
+}
+
